7bc9ac41a9662a45ffe44246203618df3f18485d
Boon Han <cnboonhan94@gmail.com>
2020-01-21 16:43:53 +0800

    add normalization of radian angles

--- rmf_core_tools/shell_mobile_robot.py
+++ rmf_core_tools/shell_mobile_robot.py
@@ -2,8 +2,11 @@ from __future__ import print_function
 import yaml
 import os
 import rclpy
+import numpy as np
+import time
+from threading import Thread
 
-class ShellRobot:
+class ShellMobileRobot:
     def __init__(self, robot_name):
         self.node = rclpy.create_node("shell_" + robot_name)
         dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -36,13 +39,31 @@ class ShellRobot:
         # Robot resumes movement, simulating the removal of temporary obstructions
         raise NotImplementedError
     
+    def normalize_angle(self, yaw):
+        angle = yaw % (2 * np.pi)
+        angle = ( angle + 2 * np.pi ) % (2 * np.pi)
+        if (angle > np.pi):
+            angle -= 2 * np.pi
+        return angle
+    
     def rotate(self, yaw):
         # Spins robot to the specified angle
-        raise NotImplementedError
+        yaw_now = self.details["state"]["yaw"]
+        hz = self.details["config"]["update_rate"]
+        thread = Thread(target = self._rotate_thread, args = (yaw, hz))
+        thread.start()
+
+def _rotate_thread(self, yaw, hz):
+        # Thread that spins robot asynchronously 
 
     def move(self, pos):
         # Translates the robot to the specified position
+        # x_now = self.details["state"]["x"]
+        # y_now = self.details["state"]["y"]
+        # pos_now = np.array([x_now, y_now])
+        # d_vec = pos - pos_now
         raise NotImplementedError
+
     
     def get_state(self):
         # Get the current status of the robot in a human-readable printout
@@ -70,8 +91,8 @@ class ShellRobot:
 def main(args=None):
     rclpy.init(args=args)
     robot_name = "magni" # TODO: Replace with parameter from ROS2
-    robot = ShellRobot(robot_name)
-    robot.get_state()
+    robot = ShellMobileRobot(robot_name)
+    # robot.rotate(2)
 
 if __name__ == '__main__':
     main()
